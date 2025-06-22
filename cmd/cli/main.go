aa94df542468600a66e9c9f2329e0deb81ffb38f
kashuvo <shuvo.official2021@gmail.com>
2025-06-23 01:27:42 +0600

    feat: add read/write functionality in wal

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,7 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+	"walstore/internal/wal"
+)
+
+var (
+	insertOp = 1
+	updateOp = 2
+	deleteOp = 3
+)
+
+type Record struct {
+	Op    int    `json:"op"`
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
 
 func main() {
-	fmt.Println("Write Ahead Log (WAL) implementation in Go")
+	defaultConfig := wal.CreateDefaultConfig("/home/shuvo/logs")
+	walog, err := wal.StartLogger(defaultConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	entries := []Record{
+		{Op: insertOp, Key: "key1", Value: "value1"},
+		{Op: updateOp, Key: "key2", Value: "value2"},
+		{Op: deleteOp, Key: "key3", Value: "value3"},
+	}
+
+	for _, entry := range entries {
+		marshaledData, err := json.Marshal(entry)
+		if err != nil {
+			panic(err)
+		}
+
+		if err := walog.WriteRecord(marshaledData); err != nil {
+			panic(err)
+		}
+	}
+
+	time.Sleep(2 * time.Second)
+
+	writtenLogs, err := walog.ReadAllRecords()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Written log count: %d", len(writtenLogs))
+
+	for _, log := range writtenLogs {
+		logEntry := Record{}
+		if err := json.Unmarshal(log.Data, &logEntry); err != nil {
+			panic(err)
+		}
+
+		fmt.Println(log.LogSequenceNumber)
+		fmt.Println(logEntry)
+		fmt.Println("--------------------")
+	}
+
+	if err := walog.Close(); err != nil {
+		panic(err)
+	}
+
 }
--- internal/wal/wal.go
+++ internal/wal/wal.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/binary"
 	"fmt"
+	"hash/crc32"
 	"io"
 	"os"
 	"path/filepath"
@@ -19,19 +20,23 @@ var (
 	SegmentPrefix = "wal-segment-"         // Default segment file prefix
 )
 
-func StartLogger(config *Config) error {
+func StartLogger(config *Config) (*WriteAheadLog, error) {
 	if err := validateConfig(config); err != nil {
-		return err
+		return nil, err
+	}
+
+	if err := os.MkdirAll(config.Directory, 0755); err != nil {
+		return nil, err
 	}
 
 	segmentFile, segmentNumber, err := loadLastSegmentFile(config)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// seek to the end of the file to start writing new records
 	if _, err := segmentFile.Seek(0, io.SeekEnd); err != nil {
-		return fmt.Errorf("Failed to seek: %w", err)
+		return nil, fmt.Errorf("Failed to seek: %w", err)
 	}
 
 	context, cancel := context.WithCancel(context.Background())
@@ -39,29 +44,122 @@ func StartLogger(config *Config) error {
 	wal := &WriteAheadLog{
 		directory:             config.Directory,
 		currSegmentFile:       segmentFile,
-		bufferWriter:          bufio.NewWriter(segmentFile),
 		currSegmentNumber:     segmentNumber,
+		maxFileSize:           config.MaxFileSize,
+		maxSegments:           config.MaxSegments,
+		enableForceSync:       config.EnableForceSync,
 		lastLogSequenceNumber: 0,
+		bufferWriter:          bufio.NewWriter(segmentFile),
 		syncTimer:             time.NewTimer(SyncInterval),
-
-		maxFileSize:     config.MaxFileSize,
-		maxSegments:     config.MaxSegments,
-		enableForceSync: config.EnableForceSync,
-		context:         context,
-		cancel:          cancel,
+		context:               context,
+		cancel:                cancel,
 	}
 
 	if wal.lastLogSequenceNumber, err = wal.getLastLogSequenceNumber(); err != nil {
-		return fmt.Errorf("Failed to get last log sequence number: %w", err)
+		return nil, fmt.Errorf("Failed  getting lsn: %w", err)
 	}
 
 	go wal.syncPeriodically()
 
+	return wal, nil
+}
+
+func (wal *WriteAheadLog) ReadAllRecords() ([]*pb.WalRecord, error) {
+	file, err := os.OpenFile(wal.currSegmentFile.Name(), os.O_RDONLY, 0644)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open WAL segment file: %w", err)
+	}
+	defer file.Close()
+	var walRecords []*pb.WalRecord
+
+	for {
+		var recordSize int32
+		// Read the size of the next record
+		if err := binary.Read(file, binary.LittleEndian, &recordSize); err != nil {
+			if err == io.EOF {
+				// End of file reached, break the loop
+				break
+			}
+			return walRecords, err
+		}
+
+		data := make([]byte, recordSize)
+		// Read the record data
+		if _, err := io.ReadFull(file, data); err != nil {
+			return walRecords, err
+		}
+
+		var record pb.WalRecord
+		if err := gpb.Unmarshal(data, &record); err != nil {
+			return walRecords, err
+		}
+
+		walRecords = append(walRecords, &record)
+	}
+
+	return walRecords, nil
+}
+
+func (wal *WriteAheadLog) WriteRecord(data []byte) error {
+	wal.lock.Lock()
+	defer wal.lock.Unlock()
+
+	// TODO: Log rotation
+	logSeqNumber := wal.lastLogSequenceNumber + 1
+
+	newRecord := &pb.WalRecord{
+		Data:              data,
+		LogSequenceNumber: logSeqNumber,
+		Timestamp:         time.Now().UnixNano(),
+		Checksum:          crc32.ChecksumIEEE(append(data, byte(logSeqNumber))),
+	}
+
+	err := wal.writeToBuffer(newRecord)
+	if err != nil {
+		return err
+	}
+	wal.lastLogSequenceNumber = logSeqNumber
 	return nil
 }
 
+func (wal *WriteAheadLog) Close() error {
+	// Stop the periodic sync timer
+	wal.cancel()
+	// Sync before closing
+	if err := wal.Sync(); err != nil {
+		return err
+	}
+	// Close the current segment file
+	return wal.currSegmentFile.Close()
+}
+
+func (wal *WriteAheadLog) writeToBuffer(record *pb.WalRecord) error {
+	marshaledRecord, err := gpb.Marshal(record)
+	if err != nil {
+		return fmt.Errorf("failed to marshal record: %w", err)
+	}
+	recordSize := int32(len(marshaledRecord))
+	// write the record size to the buffer
+	if err := binary.Write(wal.bufferWriter, binary.LittleEndian, recordSize); err != nil {
+		return fmt.Errorf("failed to write record size: %w", err)
+	}
+	// write the marshaled record to the buffer
+	_, err = wal.bufferWriter.Write(marshaledRecord)
+
+	return err
+}
+
+// TODO: Return one record only
 func (wal *WriteAheadLog) getLastLogSequenceNumber() (uint64, error) {
-	panic("unimplemented")
+	records, err := wal.ReadAllRecords()
+	if err != nil {
+		return 0, err
+	}
+	if len(records) == 0 {
+		return 0, nil // No records found, return 0
+	}
+	lastRecord := records[len(records)-1]
+	return lastRecord.LogSequenceNumber, nil
 }
 
 func (wal *WriteAheadLog) syncPeriodically() {
@@ -151,56 +249,4 @@ func createNewSegmentFile(dir string, segmentId int) (*os.File, error) {
 	}
 
 	return file, nil
-}
-
-func (wal *WriteAheadLog) ReadAllRecords() ([]*pb.WalRecord, error) {
-	file, err := os.OpenFile(wal.currSegmentFile.Name(), os.O_RDONLY, 0644)
-	if err != nil {
-		return nil, fmt.Errorf("failed to open WAL segment file: %w", err)
-	}
-	defer file.Close()
-	var walRecords []*pb.WalRecord
-
-	for {
-		var recordSize int32
-		// Read the size of the next record
-		if err := binary.Read(file, binary.LittleEndian, &recordSize); err != nil {
-			if err == io.EOF {
-				// End of file reached, break the loop
-				break
-			}
-			return walRecords, err
-		}
-
-		data := make([]byte, recordSize)
-		// Read the record data
-		if _, err := io.ReadFull(file, data); err != nil {
-			return walRecords, err
-		}
-
-		var record pb.WalRecord
-		if err := gpb.Unmarshal(data, &record); err != nil {
-			return walRecords, err
-		}
-
-		walRecords = append(walRecords, &record)
-	}
-
-	return walRecords, nil
-}
-
-func (wal *WriteAheadLog) Write(record pb.WalRecord) error {
-	marshaledRecord, err := gpb.Marshal(&record)
-	if err != nil {
-		return fmt.Errorf("failed to marshal record: %w", err)
-	}
-	recordSize := int32(len(marshaledRecord))
-	// write the record size to the buffer
-	if err := binary.Write(wal.bufferWriter, binary.LittleEndian, recordSize); err != nil {
-		return fmt.Errorf("failed to write record size: %w", err)
-	}
-	// write the marshaled record to the buffer
-	_, err = wal.bufferWriter.Write(marshaledRecord)
-
-	return err
 }
